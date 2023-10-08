e0589badbc1855bce8a1bf8db97737dfa5a9b5d8
Bill Stock <bill@wrsdevelopment.com>
2023-10-07 18:38:54 -0600

    Update DynamoDB Schema

--- src/recommendations/recommendations.py
+++ src/recommendations/recommendations.py
@@ -8,12 +8,31 @@ def lambda_handler(event, context):
     print(json.dumps(event["body"]))
 
     # Get prompt text string from event
-    print("Decoded Body")
-    requestBody = urllib.parse.unquote(event["body"])
-    print(requestBody)
+    # print("Decoded Body")
+    # requestBody = urllib.parse.unquote(event["body"])
+    # print(requestBody)
 
-    query_result = ""
+    query_result = {}
 
+    dynamodb = boto3.client("dynamodb")
+
+    response = dynamodb.query(
+        TableName="TimothyRecommendationsTable",
+        KeyConditionExpression="Classification = :classification",
+        ExpressionAttributeValues={":classification": {"S": "Prayer"}},
+    )
+    print(response["Items"])
+    query_result["Prayers"] = response["Items"]
+
+    response = dynamodb.query(
+        TableName="TimothyRecommendationsTable",
+        KeyConditionExpression="Classification = :classification",
+        ExpressionAttributeValues={":classification": {"S": "Scripture"}},
+    )
+    print(response["Items"])
+    query_result["Scripture"] = response["Items"]
+
+    print(json.dumps(query_result))
     # Check answer or set response if no answer
     if len(query_result) == 0:
         query_result = "Unable to answer your question at this time.  Please try again later or ask another question."
